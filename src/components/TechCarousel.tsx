62a15baa9867513eab916dfa218dc03183d5d2c2
ydj515 <ydj515@hanmail.net>
2025-07-29 20:00:38 +0900

    fix: carousel 수정

--- src/components/TechCarousel.tsx
+++ src/components/TechCarousel.tsx
@@ -1,13 +1,30 @@
+'use client';
+
 import Image from 'next/image';
 import { logos } from '@/constants';
 
 const TechCarousel = () => {
+  // Duplicate the icons for a seamless loop
+  const duplicatedIcons = [...logos, ...logos];
+
   return (
-    <div className="w-full overflow-hidden">
-      <div className="flex animate-scroll">
-        {[...logos, ...logos].map((logo, index) => (
-          <div key={`${logo.alt}-${index}`} className="flex-shrink-0 mx-4">
-            <Image src={logo.src} alt={logo.alt} width={64} height={64} />
+    <div
+      className="w-full max-w-4xl mx-auto overflow-hidden"
+      style={{
+        maskImage: 'linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)',
+        WebkitMaskImage: 'linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)',
+      }}
+    >
+      <div className="flex w-max animate-scroll">
+        {duplicatedIcons.map((icon, index) => (
+          <div key={index} className="flex-shrink-0 px-4">
+            <Image
+              src={`${icon.src}`}
+              alt={icon.alt}
+              width={64}
+              height={64}
+              className="mx-auto"
+            />
           </div>
         ))}
       </div>
